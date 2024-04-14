bfbd4f98031fa674026d36b46067011b90a63723
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-04-14 22:12:53 +0530

    Added pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,228 @@
+export default function pricing() {
+  return (
+    <div className="z-10 mt-36 max-w-[1300px] mx-auto p-8">
+      <h2>Cloud Gaming &amp; Design</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-10">
+        <a
+          className="items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity relative group block p-2 h-full w-full"
+          tabIndex={0}
+          role="link"
+        >
+          <div className="flex flex-col justify-between rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <div className="relative z-50">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-4">
+                  Competitor - <span className="text-blue-300">$</span>40 /
+                  Month
+                </h4>
+                <p className="mt-8 text-zinc-400 tracking-wide text-sm">
+                  Perfect for the competitive gamer. League of Legends,
+                  Counter-Strike, Fortnite, and more run seamlessly on a 1GB
+                  fiber connection. Speed is the name of the game for this
+                  package and is our best bang for buck to get you up and
+                  running.
+                  <br /> <br />
+                  Windows 10 Home <br />- Nvidia GTX 1080 <br />- Intel Xeon 3.1
+                  GHz 8-Core <br />- 12 GB RAM <br />- 512 GB SSD <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto px-4 mb-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-white rounded-md h-10 font-medium shadow-input bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)] !w-auto mt-8 px-6"
+                type="submit"
+              >
+                <span className="text-neutral-300 text-sm">Rent Now</span>
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
+              </button>
+            </div>
+          </div>
+        </a>
+        <a
+          className="items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity relative group block p-2 h-full w-full"
+          tabIndex={0}
+          role="link"
+        >
+          <div className="flex flex-col justify-between rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <div className="relative z-50">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-4">
+                  Premium - <span className="text-blue-300">$</span>65 / Month
+                </h4>
+                <p className="mt-8 text-zinc-400 tracking-wide text-sm">
+                  Ramp up your gaming and productivity with our Premium setup.
+                  Cyberpunk 2077, Elden Ring, Abode Premier Pro, Blender, and
+                  more are available including Nvidia Ray Tracing and DLSS.
+                  Quality is the focus of our Premium package and we are sure
+                  that it will deliver.
+                  <br /> <br />
+                  Windows 10 Home <br />- Nvidia RTX 3070 Ti <br />- AMD EPYC
+                  3.7 GHz 8-Core <br />- 28 GB RAM <br />- 512 GB SSD <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto px-4 mb-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-white rounded-md h-10 font-medium shadow-input bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)] !w-auto mt-8 px-6"
+                type="submit"
+              >
+                <span className="text-neutral-300 text-sm">Rent Now</span>
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
+              </button>
+            </div>
+          </div>
+        </a>
+      </div>
+      <h2 className="mt-8">Server Grade Hosting</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-10">
+        <a
+          className="items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity relative group block p-2 h-full w-full"
+          tabIndex={0}
+          role="link"
+        >
+          <div className="flex flex-col justify-between rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <div className="relative z-50">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-4">
+                  Dedicated Server Hosting (Basic) –{" "}
+                  <span className="text-blue-300">$</span>300 / Month
+                </h4>
+                <p className="mt-8 text-zinc-400 tracking-wide text-sm">
+                  Web and Server hosting that is unmatched anywhere else. 24/7
+                  support, 100% Network Uptime, MySQL, etc. The Basic plan vows
+                  to keep you connected through thick and thin with the included
+                  DDOS protection. Start out small to test the waters and
+                  upgrade at any time!
+                  <br /> <br />
+                  Ubuntu <br />- Intel Xeon 4-Core 8-Thread <br />- 16GB RAM{" "}
+                  <br />- 1 TB HDD <br />- RAID 1 Storage <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto px-4 mb-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-white rounded-md h-10 font-medium shadow-input bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)] !w-auto mt-8 px-6"
+                type="submit"
+              >
+                <span className="text-neutral-300 text-sm">Rent Now</span>
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
+              </button>
+            </div>
+          </div>
+        </a>
+        <a
+          className="items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity relative group block p-2 h-full w-full"
+          tabIndex={0}
+          role="link"
+        >
+          <div className="flex flex-col justify-between rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <div className="relative z-50">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-4">
+                  Dedicated Server Hosting (Premium) –{" "}
+                  <span className="text-blue-300">$</span>450 / Month
+                </h4>
+                <p className="mt-8 text-zinc-400 tracking-wide text-sm">
+                  Premium service and support for a host that requires it. 24/7
+                  support, 100% Network Uptime, MySQL, etc. Premium keeps you
+                  connected through thick and thin with the included DDOS
+                  protection. High level connections for a high level client.
+                  <br /> <br />
+                  Ubuntu <br />- Intel Xeon 12-Core 24-Thread <br />- 64 GB RAM{" "}
+                  <br />- 2TB HDD <br />- RAID 1 Storage <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto px-4 mb-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-white rounded-md h-10 font-medium shadow-input bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)] !w-auto mt-8 px-6"
+                type="submit"
+              >
+                <span className="text-neutral-300 text-sm">Rent Now</span>
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
+              </button>
+            </div>
+          </div>
+        </a>
+      </div>
+      <h2 className="mt-8">Cloud Computing</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-10">
+        <a
+          className="items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity relative group block p-2 h-full w-full"
+          tabIndex={0}
+          role="link"
+        >
+          <div className="flex flex-col justify-between rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <div className="relative z-50">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-4">
+                  Small DataPack - <span className="text-blue-300">$</span>20 /
+                  Month
+                </h4>
+                <p className="mt-8 text-zinc-400 tracking-wide text-sm">
+                  For your short-term demanding tasks look no further than our
+                  Small Data compute package. Offering free bandwidth and block
+                  storage, HostAI’ Small DataPack is ideal for uploading your
+                  websites and small formfactor bots.
+                  <br /> <br />
+                  Server <br />- 1 vCPU <br />- 2 GB RAM <br />- 80GB SSD
+                  Dedicated <br />- 100GB Block Storage <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto px-4 mb-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-white rounded-md h-10 font-medium shadow-input bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)] !w-auto mt-8 px-6"
+                type="submit"
+              >
+                <span className="text-neutral-300 text-sm">Rent Now</span>
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
+              </button>
+            </div>
+          </div>
+        </a>
+        <a
+          className="items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity relative group block p-2 h-full w-full"
+          tabIndex={0}
+          role="link"
+        >
+          <div className="flex flex-col justify-between rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <div className="relative z-50">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-4">
+                  Big DataPack - <span className="text-blue-300">$</span>60 /
+                  Month
+                </h4>
+                <p className="mt-8 text-zinc-400 tracking-wide text-sm">
+                  For the powerhouse projects you have been working on get all
+                  the computation power you need in our Big DataPack. Offer
+                  includes free bandwidth and additional block storage to meet
+                  all of your compute needs. Big DataPack is ideal for database
+                  processing and multi-purpose servers.
+                  <br /> <br />
+                  Server <br />- 4 vCPU <br />- 8 GB RAM <br />- 80GB SSD
+                  Dedicated <br />- 100GB Block Storage <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-auto px-4 mb-4">
+              <button
+                className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-white rounded-md h-10 font-medium shadow-input bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)] !w-auto mt-8 px-6"
+                type="submit"
+              >
+                <span className="text-neutral-300 text-sm">Rent Now</span>
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
+              </button>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
